b5039fd07047f03fddf53d0f0e6f1c909506ed6a
DudnykOleksandr <dudniksasha@gmail.com>
2017-11-29 17:58:44 +0200

    2

--- Tasks/Scripts/ViewControllers/IndexController.js
+++ Tasks/Scripts/ViewControllers/IndexController.js
@@ -1,8 +1,23 @@
 ﻿angular.module("tasks", []).controller("indexController", function ($scope) {
 
-    var vm = $scope;
-    
+    var self = $scope;
+    self.tasks = [];
+
     this.initializeController = function () {
-        vm.title = "Index Page";
+        self.title = "Index Page";
+
+        self.tasks.push("Test task");
+    }
+
+    self.addTask = function (newTask) {
+        self.tasks.push(newTask);
+    }
+
+    self.removeTask = function (task) {
+        var taskIndex = self.tasks.indexOf(task);
+        if (taskIndex!= -1)
+        {
+            self.tasks.splice(taskIndex, 1)
+        }
     }
 });
