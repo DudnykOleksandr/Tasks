4845d0ce4da44b46ad1e61acee42fb698e7dd753
DudnykOleksandr <dudniksasha@gmail.com>
2017-11-29 16:03:36 +0200

    1

---
+++ Tasks/Scripts/ViewControllers/IndexController.js
@@ -0,0 +1,8 @@
+﻿angular.module("tasks", []).controller("indexController", function ($scope) {
+
+    var vm = $scope;
+    
+    this.initializeController = function () {
+        vm.title = "Index Page";
+    }
+});
